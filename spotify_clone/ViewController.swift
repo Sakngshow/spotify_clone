d88fef235b95e8aecd85b69770195f0880e064d4
sangshow <kss95429@naver.com>
2024-07-01 15:27:52 +0900

    스포티파이 클론 시작

--- spotify_clone/ViewController.swift
+++ spotify_clone/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        print("helloworld")
         // Do any additional setup after loading the view.
     }
 
